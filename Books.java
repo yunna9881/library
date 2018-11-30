681a786d9c3d5c788415ea0505531d1872317a2c
Yunna Won <41393464+yunna9881@users.noreply.github.com>
2018-11-29 21:58:21 -0500

    Update Books.java

--- Books.java
+++ Books.java
@@ -11,7 +11,7 @@ public class Books
 	
 	private static String url = "jdbc:mysql://localhost:3306/publiclibrary";
 	private static String user = "root";
-	private static String pass = "wonyunnaA9881!";
+	private static String pass = "";
 	
 	
 	
